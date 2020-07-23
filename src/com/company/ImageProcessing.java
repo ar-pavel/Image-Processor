56e682ddb1210624e7e8b01a3c8a3d0c592d9402
ar-pavel <atikur1998@yahoo.com>
2020-07-23 17:28:43 +0600

    Code style improved

--- src/com/company/ImageProcessing.java
+++ src/com/company/ImageProcessing.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 import java.io.File;
 
 public class ImageProcessing {
+    public static Image image;
+    public  static int height;
+    public  static int width;
 
     // Image reader
     public static String readImage() {
@@ -32,57 +35,8 @@ public class ImageProcessing {
         }
     }
 
-
-    public static void main(String args[]) {
-
-        // load an image
-        Image image = new Image(readImage());
-
-        // print height and width
-        System.out.println("Height is " + image.getHeight() + " and width is " + image.getWidth());
-
-        int height = image.getHeight();
-        int width = image.getWidth();
-
-
-        // create another image for output
-        // it has the same size as original image
-
-        Image red = new Image(height, width);
-
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int r = image.getPixelR(x, y);
-                red.setPixel(x, y, r, r, r);
-
-            }
-        }
-//        red.save("r.png");
-
-        Image green = new Image(height, width);
-
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int g = image.getPixelG(x, y);
-                green.setPixel(x, y, g, g, g);
-
-            }
-        }
-//        green.save("g.png");
-
-        Image blue = new Image(height, width);
-
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int b = image.getPixelB(x, y);
-                blue.setPixel(x, y, b, b, b);
-
-            }
-        }
-//        blue.save("b.png");
-
+    public void generateGrayScale(){
         Image grayscale = new Image(height, width);
-
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -96,11 +50,81 @@ public class ImageProcessing {
         }
 
         // save the output image
-//        grayscale.save("grayscale.png");
+        grayscale.save("grayscale.png");
+    }
 
-        Image resizedImage = image.resizedImage(1800, 3400);
+    public void generateBlue(){
+        Image blue = new Image(height, width);
 
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int b = image.getPixelB(x, y);
+                blue.setPixel(x, y, b, b, b);
+
+            }
+        }
+        blue.save("b.png");
+    }
+
+    public void generateRed(){
+        Image red = new Image(height, width);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int r = image.getPixelR(x, y);
+                red.setPixel(x, y, r, r, r);
+
+            }
+        }
+        red.save("r.png");
+    }
+
+    public void generateGreen(){
+        Image green = new Image(height, width);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int g = image.getPixelG(x, y);
+                green.setPixel(x, y, g, g, g);
+
+            }
+        }
+        green.save("g.png");
+
+    }
+
+    public void resizedImage(int newHeight, int newWidth){
+        Image resizedImage = image.resizedImage(newHeight, newWidth);
         resizedImage.save("resized Image.png");
+    }
+
+    public void processToAssignment(){
+        
+
+    }
+
+
+    public static void main(String args[]) {
+
+        // load an image
+        image = new Image(readImage());
+
+        // print height and width
+        System.out.println("Height is " + image.getHeight() + " and width is " + image.getWidth());
+
+        height = image.getHeight();
+        width = image.getWidth();
+
+        // create another image for output
+        // it has the same size as original image
+
+        new ImageProcessing().generateGrayScale();
+        new ImageProcessing().generateRed();
+        new ImageProcessing().generateBlue();
+        new ImageProcessing().generateGreen();
+
+        new ImageProcessing().resizedImage(1400, 1800);
+
 
 
 
